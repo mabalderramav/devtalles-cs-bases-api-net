63103cf6ec8fd5b15647e985e0836c3dd1810e83
Teddy Paz <pazteddy@gmail.com>
2025-05-12 15:17:50 -0400

    Add IProduct interface and Product class implementation; fix typos in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,12 @@
   {
     //Declaración explicíta
     int quantity = 5;
-    string message = "Hello Wolrd";
+    string message = "Hello World";
     decimal price = 19.99m;
     WriteLine($" Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
     // Declaración con var
     var gretting = "Hola";
-    var porcentage = 20.00m;
-    WriteLine($" Saludo: {gretting}, Porcentaje: {porcentage}");
+    var percentage = 20.00m;
+    WriteLine($" Saludo: {gretting}, Porcentaje: {percentage}");
   }
 }
