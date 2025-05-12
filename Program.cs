aedf8e76f187e4516c33a6af7449b89815f2f099
Teddy Paz <pazteddy@gmail.com>
2025-05-12 14:42:07 -0400

    Add initial project files including .gitignore, solution, and C# program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+  static void Main()
+  {
+    //Declaración explicíta
+    int quantity = 5;
+    string message = "Hello Wolrd";
+    decimal price = 19.99m;
+    WriteLine($" Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
+    // Declaración con var
+    var gretting = "Hola";
+    var porcentage = 20.00m;
+    WriteLine($" Saludo: {gretting}, Porcentaje: {porcentage}");
+  }
+}
